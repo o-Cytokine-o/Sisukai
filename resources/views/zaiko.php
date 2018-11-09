4ee00b65e43188a0781f3c31d3851b6011a60b04
unknown <B7046@oic.osaka.jp>
2018-11-09 14:30:30 +0900

    ume 11/09

--- resources/views/zaiko.php
+++ resources/views/zaiko.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -39,7 +39,78 @@
             </div>
             <div class="contents col-xl-10 col-lg-10 col-md-9 col-sm-9 col-xs-9 wrap">
                 <!---- コンテンツ ----->
-                
+                    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <h1>在庫画面</h1>
+<div class="main1">
+    <div class="text1">
+        <input id="sbox1" id="s" name="s" type="search" placeholder="" />
+        <input id="sbtn1" type="submit" value="検索" />
+        <p>
+            商品名 -----------------
+        </p>
+    </div>
+    <div class="text2">
+        <h2>在庫情報一覧</h2>
+        
+        <div class="text3">
+            <table border="1">
+            <tr>
+                <th class="a1">商品名</th>
+                <th class="b1">在庫数量</th>
+                <th class="c1">安全在庫数量</th>
+                <th class="d1">商品単価</th>
+                <th class="e1">在庫残高</th>
+            </tr>
+            <tr>
+                <td>モンハン</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            </table>
+        </div>
+
+ <!--       
+        <ul class="text4">
+            <li>-----------------------------------------------------</li>
+        </ul>
+-->
+        <button type="submit" class="button">クリア</button>
+    </div>
+</div>
+<div class="main2">
+        <h3>安全在庫数量以下</h3>
+    <div class="text5">
+        <table border="1">
+            <tr>
+            <th class="a2">商品コード</th>
+            <th class="b2">商品名</th>
+            <th class="c2">在庫数量</th>
+            <th class="d2">安全在庫数量</th>
+            <th class="e2">商品単価</th>
+            <th class="f2">在庫残高</th>
+            </tr>
+            <tr>
+            <td>1</td>
+            <td>モンハン</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            </tr>
+        </table>
+        </div>
+<!--    
+        <ul class="text6">
+            <li>------------------------------------------------------------------</li>
+        </ul>
+-->
+</div>
             
                 
                 <!---- コンテンツend ----->
