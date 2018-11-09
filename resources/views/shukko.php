3e34c0ea8624aee132c5ef1b4c8b3221c8ca4e54
o-Cytokine-o <b7072@oic.jp>
2018-11-09 18:34:47 +0900

    OM11/9 vol3

--- resources/views/shukko.php
+++ resources/views/shukko.php
@@ -39,53 +39,39 @@
             </div>
             <div class="contents col-xl-10 col-lg-10 col-md-9 col-sm-9 col-xs-9 wrap">
                 <!---- コンテンツ ----->
-                  <h1>出庫画面</h1>
-    <div class="data">
-        <div class="br"> 
-        <span>商品名</span>
-        <input class="text" type="text"><br>
-</div>
-    <div class="br">
-        <span>商品番号</span>
-        <input type="text"><br>
-</div>
-    <div class="br">
-        <span>入庫先名</span>
-        <input type="text"><br>
-</div>
-    <div class="br">
-        <span>入庫先番号</span>
-        <input type="text"><br>
-</div>
-    <div class="br">
-        <span>入庫予定日</span>
-        <input type="text"><br>
-</div>
-    <div class="br">
-        <span>仕入価格</span>
-        <input type="text"><br>
-</div>
-    <div class="br">
-        <span>数量</span>
-        <input type="text"><br>
-</div>
-<button type="button">追加</button>
-<table border="1">
-<tr>
-<th>商品名</th>
-<th>数量</th>
-<th></th>
-</tr>
-<tr>
-<td>商品名</td>
-<td>商品名２</td>
-<td><button type="button">x</button>
-</tr>
-</table>
-<button type="button">確定</button>
-   </div>
-            
-                
+                <div class="content-wrap">
+                        <table class="serch-box table">
+                            <!---検索ボックス--->
+                            <thead class="serch-box-thead">
+                                <tr>
+                                    <th>
+                                        <form class="serch-form">
+                                            <input type="text" name="serch-form">
+                                        </form>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>商品名</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <!-- 注文一覧 -->
+                        <table class="table shukko-box">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>出庫一覧</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>追加された商品</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                </div>         
                 <!---- コンテンツend ----->
                 </div>
         </div>
